3529d576a418a6d963978e24153afef09e6f7183
Chris Baucom <crbaucom@gmail.com>
2018-12-19 23:33:08 -0500

    refactor with arrow functions

--- notes-app.js
+++ notes-app.js
@@ -14,7 +14,7 @@ const filters = {
 
 renderNotes(notes, filters);
 
-document.querySelector("#create-note").addEventListener("click", function(e) {
+document.querySelector("#create-note").addEventListener("click", e => {
   const id = uuidv4();
   const timestamp = moment().valueOf();
 
@@ -38,34 +38,19 @@ document.querySelector("#create-note").addEventListener("click", function(e) {
 //   console.log("Deleted all notes");
 // });
 
-document.querySelector("#search-text").addEventListener("input", function(e) {
+document.querySelector("#search-text").addEventListener("input", e => {
   filters.searchText = e.target.value;
   renderNotes(notes, filters);
 });
 
-document.querySelector("#filter-by").addEventListener("change", function(e) {
+document.querySelector("#filter-by").addEventListener("change", e => {
   filters.sortBy = e.target.value;
   renderNotes(notes, filters);
 });
 
-window.addEventListener("storage", function(e) {
+window.addEventListener("storage", e => {
   if (e.key === "notes") {
     notes = JSON.parse(e.newValue);
     renderNotes(notes, filters);
   }
 });
-
-// const now = moment();
-// now.subtract(1, "week");
-// console.log(now.format("MMMM Do, YYYY"));
-// console.log(now.fromNow());
-
-// const nowTimestamp = now.valueOf();
-// console.log(moment(nowTimestamp).toString());
-
-// const birthday = moment();
-// birthday
-//   .year(1983)
-//   .month(9)
-//   .date(1);
-// console.log(birthday.format("MMM D, YYYY"));
